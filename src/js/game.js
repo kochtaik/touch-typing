16f04e5977ab84a5df9ceff1fb4a22390b169635
kochtaik <kochtaik@gmail.com>
2020-12-02 14:03:32 +0300

    feat: improve characters highlight

--- src/js/game.js
+++ src/js/game.js
@@ -4,6 +4,7 @@ class Game {
     this.text = text;
     this.mistakes = 0;
     this.inputIndex = 0;
+    this.input = '';
     this.start = this.start.bind(this);
     this.elements = {
       startBtn: document.querySelector('#start'),
@@ -15,7 +16,7 @@ class Game {
   start() {
     // Game.startCountdown()
     const { inputField, startBtn } = this.elements;
-    this.advance();
+    this.advance(true);
     inputField.focus();
     startBtn.disabled = true;
     inputField.addEventListener('input', () => {
@@ -24,23 +25,50 @@ class Game {
     });
   }
 
-  advance() {
+  advance(isCorrect) {
     const { inputIndex, text } = this;
     const { textElem } = this.elements;
-    const unhighlightedChar = text.charAt(inputIndex);
-    const highlightedChar = `<span class="text__content--char-highlighted">${unhighlightedChar}</span>`;
+    const unhighlightedChar = this.getCurrentChar();
+    const highlightedChar = Game.highlightCurrentChar(unhighlightedChar, isCorrect);
     const textCopy = [...text];
     textCopy.splice(inputIndex, 1, highlightedChar);
     textElem.innerHTML = textCopy.join('');
   }
 
+  getCurrentChar() {
+    const { inputIndex, text } = this;
+    return text.charAt(inputIndex);
+  }
+
+  static highlightCurrentChar(char, isCorrect) {
+    let classPostfix;
+    if (isCorrect) {
+      classPostfix = '--char-correct';
+    } else classPostfix = '--char-mistaked';
+    return `<span class="text__content${classPostfix}">${char}</span>`;
+  }
+
   validateInput(char) {
     const requiredChar = this.text[this.inputIndex];
-    console.log(requiredChar === char);
-    if (requiredChar === char || (requiredChar === 'ё' && char === 'е')) {
+    const inputText = this.elements.inputField.value;
+    const enteredCharIndex = inputText.length - 1;
+    const requiredCharIndex = this.inputIndex;
+    console.log('required:', requiredCharIndex);
+    console.log('entered:', enteredCharIndex);
+    if ((requiredCharIndex === enteredCharIndex) && (requiredChar === char)) {
       this.inputIndex += 1;
-      this.advance();
+      this.advance(true);
+    } else if (inputText.length < this.input.length) {
+      this.inputIndex -= 1;
+      const { inputField } = this.elements;
+      const enteredChar = inputField.value[inputField.value.length - 1];
+      this.validateInput(enteredChar);
+      console.log('ready')
+    } else {
+      this.advance(false);
     }
+    console.log('inputText', inputText.length)
+    console.log('input', this.input.length)
   }
 }
 
